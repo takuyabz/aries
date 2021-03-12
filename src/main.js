6353db19dea2764a8e7b671aff20d9ef6ed0ec8e
sato <business@satotakuya.jp>
2021-03-12 23:21:18 +0900

    second commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import Amplify, * as AmplifyModules from "aws-amplify"
+import { AmplifyPlugin } from "aws-amplify-vue"
+import aws_exports from "./aws-exports"
+import router from './router'
+Amplify.configure(aws_exports)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
